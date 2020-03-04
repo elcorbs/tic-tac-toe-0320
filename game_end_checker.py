6cc6dbd4f8713ce20306b9e58484ea5087ee7400
elcorbs <emma@madetech.com>
2020-03-04 17:16:51 +0000

    Finish win cases for the game checker

--- game_end_checker.py
+++ game_end_checker.py
@@ -1,13 +1,22 @@
 class GameEndChecker():
   def is_draw(self, board):
     return None not in board[0] + board[1] + board[2]
-    
-  def who_won(self,board):
-    first_row_won = board[0][0] and board[0][0] == board[0][1] == board[0][2]
-    second_row_won = board[1][0] and board[1][0] == board[1][1] == board[1][2]
 
-    if first_row_won:
+  def who_won(self,board):
+    for squares in board.values():
+      if self.squares_equal(squares[0], squares[1], squares[2]):
+        return squares[0]
+    
+    for i in range(len(board[0])):
+      if self.squares_equal(board[0][i], board[1][i], board[2][i]):
+        return board[0][i]
+
+    if self.squares_equal(board[0][0], board[1][1], board[2][2]):
       return board[0][0]
-    if second_row_won:
-      return board[1][0]
+
+    if self.squares_equal(board[0][2], board[1][1], board[2][0]):
+      return board[0][2]
     return None
+  
+  def squares_equal(self, one, two, three):
+    return one and one == two == three
--- test_game_end_checker.py
+++ test_game_end_checker.py
@@ -1,28 +1,54 @@
 import unittest
 from game_end_checker import GameEndChecker
 
-class TestGameEndChecker(unittest.TestCase):  
+class TestGameEndChecker(unittest.TestCase):
+  def setUp(self):
+    self.game_end_checker = GameEndChecker()
+
   def test_checker_recognises_draw(self):
     board = { 0: ['X', 'O', 'X'], 1: ['O', 'X', 'X'], 2: ['O', 'X', 'O'] }
-    game_end_checker = GameEndChecker()
-    self.assertEqual(game_end_checker.is_draw(board), True)
+    self.assertEqual(self.game_end_checker.is_draw(board), True)
 
   def test_checker_recognises_not_draw(self):
     board = { 0: ['X', 'O', 'X'], 1: [None, 'X', 'X'], 2: ['O', 'X', 'O'] }
-    game_end_checker = GameEndChecker()
-    self.assertEqual(game_end_checker.is_draw(board), False)
+    self.assertEqual(self.game_end_checker.is_draw(board), False)
 
   def test_if_player_X_won_first_row_state_is_won(self):
     board = { 0: ['X', 'X', 'X'], 1: ['O', 'O', None], 2: [None, None, None] }
-    game_end_checker = GameEndChecker()
-    self.assertEqual(game_end_checker.who_won(board), 'X')
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
 
   def test_if_player_O_won_first_row_state_is_won(self):
     board = { 0: ['O', 'O', 'O'], 1: ['X', 'X', None], 2: [None, None, None] }
-    game_end_checker = GameEndChecker()
-    self.assertEqual(game_end_checker.who_won(board), 'O')
+    self.assertEqual(self.game_end_checker.who_won(board), 'O')
 
   def test_if_player_X_won_second_row_state_is_won(self):
     board = { 0: ['O', 'O', None], 1: ['X', 'X', 'X'], 2: [None, None, None] }
-    game_end_checker = GameEndChecker()
-    self.assertEqual(game_end_checker.who_won(board), 'X')
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
+
+  def test_if_player_O_won_second_row_state_is_won(self):
+    board = { 0: ['O', 'O', None], 1: ['O', 'O', 'O'], 2: [None, None, None] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'O')
+
+  def test_if_player_X_won_third_row_state_is_won(self):
+    board = { 0: ['O', 'O', None], 1: [None, None, None], 2: ['X', 'X', 'X'] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
+  
+  def test_if_player_X_won_first_column_state_is_won(self):
+    board = { 0: ['X', 'O', None], 1: ['X', None, None], 2: ['X', 'O', 'O'] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
+
+  def test_if_player_X_won_second_column_state_is_won(self):
+    board = { 0: ['O', 'X', None], 1: ['O', 'X', None], 2: [None, 'X', 'O'] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
+  
+  def test_if_player_X_won_third_column_state_is_won(self):
+    board = { 0: ['O', None, 'X'], 1: ['O', None, 'X'], 2: [None, 'O', 'X'] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
+
+  def test_if_player_X_won_backward_diagonal_state_is_won(self):
+    board = { 0: ['X', None, 'O'], 1: ['O', 'X', None], 2: [None, 'O', 'X'] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
+
+  def test_if_player_X_won_forward_diagonal_state_is_won(self):
+    board = { 0: [None, 'O', 'X'], 1: ['O', 'X', None], 2: ['X', None, 'O'] }
+    self.assertEqual(self.game_end_checker.who_won(board), 'X')
